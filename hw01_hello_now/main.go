bd002fe73d94f87b5a39ef892276fe5158e007c3
viktor.ledenev <viktor.ledenev@inplatlabs.ru>
2020-08-31 13:25:22 +0300

    Try to trigger travis

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -9,11 +9,11 @@ import (
 )
 
 func main() {
-	layout := "2006-01-02 03:04:05 -0700 MST"
+	timeLayout := "2006-01-02 03:04:05 -0700 MST"
 	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
-	fmt.Println("current time:", time.Now().Format(layout))
-	fmt.Println("exact time:", ntpTime.Format(layout))
+	fmt.Println("current time:", time.Now().Format(timeLayout))
+	fmt.Println("exact time:", ntpTime.Format(timeLayout))
 }

8401110c840cc328ed152d37d5f9cad8183278d1
viktor.ledenev <viktor.ledenev@inplatlabs.ru>
2020-08-31 12:24:11 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	layout := "2006-01-02 03:04:05 -0700 MST"
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	fmt.Println("current time:", time.Now().Format(layout))
+	fmt.Println("exact time:", ntpTime.Format(layout))
 }

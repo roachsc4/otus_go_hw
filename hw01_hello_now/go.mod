02a16c77f42ff5073335bd969fbf0c495fd6ee8f
viktor.ledenev <viktor.ledenev@inplatlabs.ru>
2020-09-01 10:12:38 +0300

    Fixed review comments for hw1: 1. TimeLayout is const now 2. golangci-lint deleted from go.mod, new dependencies added by go mod tidy

--- hw01_hello_now/go.mod
+++ hw01_hello_now/go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	bou.ke/monkey v1.0.2
 	github.com/beevik/ntp v0.3.0
-	github.com/golangci/golangci-lint v1.30.0 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 )
--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -8,12 +8,13 @@ import (
 	"github.com/beevik/ntp"
 )
 
+const TimeLayout = "2006-01-02 03:04:05 -0700 MST"
+
 func main() {
-	timeLayout := "2006-01-02 03:04:05 -0700 MST"
 	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
-	fmt.Println("current time:", time.Now().Format(timeLayout))
-	fmt.Println("exact time:", ntpTime.Format(timeLayout))
+	fmt.Println("current time:", time.Now().Format(TimeLayout))
+	fmt.Println("exact time:", ntpTime.Format(TimeLayout))
 }

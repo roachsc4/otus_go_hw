1fdabfe48f5f85ec4359412ab0a463d586d6a73d
viktor.ledenev <viktor.ledenev@inplatlabs.ru>
2020-10-21 23:32:32 +0300

    HW6 is done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,38 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here
-	return nil
+	if in == nil {
+		return nil
+	}
+	/*
+		The main principe of controlling stages is using sentinel (intermediate) channels.
+		It helps to make controllable (via special "done" channel) channels returned by stages.
+	*/
+	for _, stage := range stages {
+		// Create sentinel channel.
+		sentinelChannel := make(chan interface{})
+		// Start wrapping goroutine.
+		go func(ch In) {
+			defer close(sentinelChannel)
+			for {
+				// Try to:
+				// 1) receive data from input channel and put it in sentinel channel;
+				// 2) or receive signal from "done" channel.
+				// Return in case of closed input channel or signal received from "done".
+				select {
+				case v, ok := <-ch:
+					if !ok {
+						return
+					}
+					sentinelChannel <- v
+				case <-done:
+					return
+				}
+			}
+		}(in)
+		// Sentinel channel turns into input channel for stage,
+		// and output channel of stage becomes new input for next wrapping goroutine.
+		in = stage(sentinelChannel)
+	}
+	return in
 }

7c49aac6d49c2588801c61c9045fa3e7c666a348
viktor.ledenev <viktor.ledenev@inplatlabs.ru>
2020-11-16 21:53:04 +0300

    HW8 done

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,10 +1,68 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]string
+
+func processBytesValue(value []byte) []byte {
+	value = bytes.TrimRight(value, " \t\n")
+	value = bytes.Replace(value, []byte("\x00"), []byte("\n"), -1)
+	return value
+}
+
+func readFirstLineOfFile(file *os.File) ([]byte, error) {
+	reader := bufio.NewReader(file)
+	value, err := reader.ReadBytes('\n')
+	if err != nil && err != io.EOF {
+		return nil, fmt.Errorf("error while reading first line of %s: %w", file.Name(), err)
+	}
+	return value, nil
+}
+
+func getValueFromFile(dir, fileName string) (string, error) {
+	if strings.Contains(fileName, "=") {
+		return "", fmt.Errorf("failed to process %s: it's name contains '='", fileName)
+	}
+	filePath := path.Join(dir, fileName)
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open file in dir: %w", err)
+	}
+	defer file.Close()
+	value, err := readFirstLineOfFile(file)
+	if err != nil {
+		return "", err
+	}
+	value = processBytesValue(value)
+
+	return string(value), nil
+}
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	fileNames, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read directory data: %w", err)
+	}
+
+	env := make(Environment, len(fileNames))
+	for _, fileName := range fileNames {
+		value, err := getValueFromFile(dir, fileName.Name())
+		if err != nil {
+			return nil, err
+		}
+		env[fileName.Name()] = value
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,39 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env
+import (
+	"os"
+	"os/exec"
+)
+
+const InvalidCommandString = 418
+
+// RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here
-	return
+	if len(cmd) < 2 {
+		return InvalidCommandString
+	}
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	for variable, value := range env {
+		if value == "" {
+			err := os.Unsetenv(variable)
+			if err != nil {
+				return -1
+			}
+		}
+		err := os.Setenv(variable, value)
+		if err != nil {
+			return -1
+		}
+	}
+
+	command.Env = os.Environ()
+	err := command.Run()
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		return exitErr.ExitCode()
+	}
+	return 0
 }

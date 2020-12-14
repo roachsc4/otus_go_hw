5ef5521c6f898dfa44755789550a752ad7b6db4b
viktor.ledenev <viktor.ledenev@inplatlabs.ru>
2020-12-14 20:42:28 +0300

    HW10: fixed comments

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -2,9 +2,7 @@ package hw10_program_optimization //nolint:golint,stylecheck
 
 import (
 	"bufio"
-	"fmt"
 	"io"
-	"regexp"
 	"strings"
 )
 
@@ -20,40 +18,21 @@ type User struct {
 
 type DomainStat map[string]int
 
-var emailRegexp = regexp.MustCompile(`.+@\w+(\.\w+)+$`)
-
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain), nil
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
+	result := make(DomainStat)
 	scanner := bufio.NewScanner(r)
-
 	for i := 0; scanner.Scan(); i++ {
 		var user User
-		if err = user.UnmarshalJSON(scanner.Bytes()); err != nil {
-			return
+		if err := user.UnmarshalJSON(scanner.Bytes()); err != nil {
+			return nil, err
 		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) DomainStat {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		if strings.Contains(user.Email, "."+domain) && emailRegexp.MatchString(user.Email) {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		if strings.Contains(user.Email, "."+domain) && strings.Contains(user.Email, "@") {
+			splittedString := strings.SplitN(user.Email, "@", 2)
+			if len(splittedString) == 2 {
+				result[strings.ToLower(splittedString[1])]++
+			}
 		}
 	}
-	return result
+
+	return result, nil
 }
